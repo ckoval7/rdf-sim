de4a3bf491adcb932ea10cd1953a5b95cfeef200
Corey Koval <ckoval7@gmail.com>
2020-07-27 17:15:04 -0400

    Prettied up web interface

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm -rf /ram/*
-cp index.html /ram/index.html
-cp -r compass /ram/compass
+cp -r ./web/* /ram/
 php -S 0.0.0.0:8080 -t /ram/ > /dev/null 2>&1 &
 python3 ./run_df_sim.py
--- web/index.php
+++ web/index.php
@@ -1,7 +1,12 @@
 <html>
 <head>
+  <link rel="icon"
+      type="image/png"
+      href="/fox.png">
+  <link rel="stylesheet" type="text/css" href="/style.css">
 </head>
 <body>
+  <?php include "header.html"; ?>
   <h3>Test transmitter 1:</h3>
   <p>Single mobile transmitter with a regular TX cycle of one minute active, three minutes idle.</p>
   <h3>RDF Receivers:</h3>
